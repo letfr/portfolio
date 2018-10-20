22b5b22239c74b9bc9573c758220c38b025940c0
Letícia Faria <leticianfaria@hotmail.com>
2018-10-20 18:55:31 -0300

    Updates

---
+++ precache-manifest.77b31ac3fe6d294b7844d1745369dd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "379c0774d7bd05bdabe71ca16d227aba",
+    "url": "/portfolio/static/media/background-intro.379c0774.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "3f9816d435608f8d90ad",
+    "url": "/portfolio/static/js/main.3f9816d4.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/portfolio/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "3f9816d435608f8d90ad",
+    "url": "/portfolio/static/css/main.10af2719.chunk.css"
+  },
+  {
+    "revision": "9cbeb3df9bf7395f2ae3e3ac6851d453",
+    "url": "/portfolio/index.html"
+  }
+];
