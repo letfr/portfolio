2a1758edebf358aa3e007f98e8b9b1882bb6a4b7
Letícia Faria <leticianfaria@hotmail.com>
2018-10-24 15:53:08 -0200

    Updates

---
+++ precache-manifest.ca681ff1b100feaf015960fb74d6615e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "916c59bf01ac8e312963aa36003fa5be",
+    "url": "/portfolio/static/media/mockup.916c59bf.png"
+  },
+  {
+    "revision": "4752ab61581065a0c2a5fce594f235a0",
+    "url": "/portfolio/static/media/background-mobile.4752ab61.jpg"
+  },
+  {
+    "revision": "f3cf96b9728221dec9328159a9bab5cd",
+    "url": "/portfolio/static/media/background-intro.f3cf96b9.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a058db2ad6633e5771a1",
+    "url": "/portfolio/static/js/main.a058db2a.chunk.js"
+  },
+  {
+    "revision": "7258aa498efb1b5b3320",
+    "url": "/portfolio/static/js/1.7258aa49.chunk.js"
+  },
+  {
+    "revision": "a058db2ad6633e5771a1",
+    "url": "/portfolio/static/css/main.1cc5fe30.chunk.css"
+  },
+  {
+    "revision": "6cbc6ed4438a04484a33b9bfe3f7ec72",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3fa12312e1b18b867136686ab0056d3f.js"
+  "/portfolio/precache-manifest.ca681ff1b100feaf015960fb74d6615e.js"
 );
 
 workbox.clientsClaim();
