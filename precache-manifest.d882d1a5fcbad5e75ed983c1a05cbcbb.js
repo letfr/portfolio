bada7b5fb43d2e037d1e559965944008ccfb92e1
Leticia Faria <leticia.faria@dasa.com.br>
2020-06-08 23:43:38 -0300

    Updates

---
+++ precache-manifest.d882d1a5fcbad5e75ed983c1a05cbcbb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2161a132215aec42278de115773cba6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b80fc548be73aff189d9",
+    "url": "/portfolio/static/css/main.41c06129.chunk.css"
+  },
+  {
+    "revision": "5feb34bc99a68667c37d",
+    "url": "/portfolio/static/js/2.4af9a4b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4af9a4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80fc548be73aff189d9",
+    "url": "/portfolio/static/js/main.c0f7ffac.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "c9f7891bc1e9b6d05934c0b7e0f81c85",
+    "url": "/portfolio/static/media/chat.c9f7891b.png"
+  },
+  {
+    "revision": "7bcb792f7c9fd681950638bdfc2bc603",
+    "url": "/portfolio/static/media/computer-girl.7bcb792f.png"
+  },
+  {
+    "revision": "2a5433b6a7465d79821937cae810f0c9",
+    "url": "/portfolio/static/media/cv-en.2a5433b6.pdf"
+  },
+  {
+    "revision": "99f7453ec5c75b79ca53c2ef09114c28",
+    "url": "/portfolio/static/media/cv-pt.99f7453e.pdf"
+  },
+  {
+    "revision": "80379ca691cd6e89d97d575c27b13141",
+    "url": "/portfolio/static/media/dashboard.80379ca6.png"
+  },
+  {
+    "revision": "7b37511926b90715e78bd5056ceb2bef",
+    "url": "/portfolio/static/media/ecommerce.7b375119.png"
+  },
+  {
+    "revision": "461f548b5218643367233d1c1d094f91",
+    "url": "/portfolio/static/media/foodmap.461f548b.png"
+  },
+  {
+    "revision": "916c59bf01ac8e312963aa36003fa5be",
+    "url": "/portfolio/static/media/mockup.916c59bf.png"
+  },
+  {
+    "revision": "4f0c0a45bc34a39d08f33d46d7747bdf",
+    "url": "/portfolio/static/media/namaste-girl.4f0c0a45.png"
+  },
+  {
+    "revision": "112d58693f44e4ae251e350215c4d706",
+    "url": "/portfolio/static/media/news-pocket.112d5869.png"
+  },
+  {
+    "revision": "fe1af7526eeada8a20b87b22c9041a1a",
+    "url": "/portfolio/static/media/social-network.fe1af752.png"
+  }
+]);
